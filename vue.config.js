64f5d455f3127be5319f742362e830b6b1a2bae0
robertzwj <robertzhangwenjie@gmail.com>
2020-05-05 09:44:41 +0800

    build(devserver): add proxy option

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: robert zhang
  * @Date: 2020-04-23 06:53:58
- * @LastEditTime: 2020-05-01 08:21:43
+ * @LastEditTime: 2020-05-05 09:35:36
  * @LastEditors: robert zhang
  * @Description: 
  * @
@@ -9,7 +9,13 @@
 module.exports = {
   devServer : {
     port: 8000,
-    open: true
+    open: true,
+    proxy: {
+      '/api/private/v1': {
+        target: "http://robert.shop.yunxiao.com",
+        changeOrigin: true //代理发送请求时，请求头中的host设置为target
+      }
+    }
   },
   pwa: {
     iconPaths: {
