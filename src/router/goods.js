167420079756bc0204e4673ad7a3f7bc52543afe
robertzwj <robertzhangwenjie@gmail.com>
2020-05-19 09:59:53 +0800

    feat(goodsparams): <!-- 添加动态参数面板 -->

--- src/router/goods.js
+++ src/router/goods.js
@@ -1,14 +1,16 @@
 /*
  * @Author: robert zhang
  * @Date: 2020-05-05 10:19:05
- * @LastEditTime: 2020-05-05 10:27:58
+ * @LastEditTime: 2020-05-18 20:48:40
  * @LastEditors: robert zhang
  * @Description: 商品管理路由
  * @
  */
 
 import GoodsCate from '@/components/goods/GoodsCate.vue'
+import GoodsParams from '@/components/goods/GoodsParams.vue'
 
 export default [
-  { path: '/categories', component: GoodsCate }
+  { path: '/categories', component: GoodsCate },
+  { path: '/params', component: GoodsParams }
 ]
