fe3e6d157540837043dd99d3391d33ff2d2aae01
robertzwj <robertzhangwenjie@gmail.com>
2020-05-31 18:20:20 +0800

    feat(goodsparam): add curd function of goodparam

--- src/components/goods/GoodsParams.vue
+++ src/components/goods/GoodsParams.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: robert zhang
  * @Date: 2020-05-18 20:48:48
- * @LastEditTime: 2020-05-19 09:45:54
+ * @LastEditTime: 2020-05-31 17:59:05
  * @LastEditors: robert zhang
  * @Description: 商品参数页面
  * @
@@ -40,14 +40,96 @@
       <el-tabs v-model="activeName" @tab-click="handleTabClick">
         <!-- 添加动态参数面板 -->
         <el-tab-pane label="动态参数" name="many">
-          <el-button type="primary" size="mini" :disabled="isBtnDisable">添加参数</el-button>
+          <el-button
+            type="primary"
+            size="mini"
+            :disabled="isBtnDisable"
+            @click="addParamDialogVisible = true"
+          >添加参数</el-button>
+          <!-- 动态参数表格 -->
+          <el-table :data="manyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  type="primary"
+                  size="mini"
+                  icon="el-icon-edit"
+                  @click="editParam(scope.row)"
+                >编辑</el-button>
+                <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
         <!-- 添加静态属性面板 -->
         <el-tab-pane label="静态属性" name="only">
-          <el-button type="primary" size="mini" :disabled="isBtnDisable">添加属性</el-button>
+          <el-button
+            type="primary"
+            size="mini"
+            :disabled="isBtnDisable"
+            @click="addParamDialogVisible = true"
+          >添加属性</el-button>
+          <el-table :data="onlyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  type="primary"
+                  size="mini"
+                  icon="el-icon-edit"
+                  @click="editParam(scope.row)"
+                >编辑</el-button>
+                <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
+    <!-- 添加参数对话框 -->
+    <el-dialog
+      :title="'添加'+ addParamDiagTitle"
+      :visible.sync="addParamDialogVisible"
+      width="30%"
+      :close="handleCloseAddParamDiag"
+    >
+      <el-form :model="paramForm" ref="paramForm" :rules="paramFormRules" label-width="80px">
+        <el-form-item :label="addParamDiagTitle" prop="attr_name">
+          <el-input v-model="paramForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addParamDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addParam">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 编辑参数对话框 -->
+    <el-dialog
+      :title="'添加'+ editParamDiagTitle"
+      :visible.sync="editParamDialogVisible"
+      width="30%"
+      :close="handleCloseEditParamDiag"
+    >
+      <el-form
+        :model="editParamForm"
+        ref="editParamForm"
+        :rules="paramFormRules"
+        label-width="80px"
+      >
+        <el-form-item :label="editParamDiagTitle" prop="attr_name">
+          <el-input v-model="editParamForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editParamDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="changeParam">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -67,7 +149,29 @@ export default {
       // 级联选择框双向绑定数组
       selectedKeys: [],
       // 当前激活的tab页名称
-      activeName: 'many'
+      activeName: 'many',
+      // 动态参数数据
+      manyTableData: [],
+      // 静态参数数据
+      onlyTableData: [],
+      // 添加参数对话框是否显示
+      addParamDialogVisible: false,
+      // 参数表单对象
+      paramForm: {
+        attr_name: ''
+      },
+      // 参数表单校验规则
+      paramFormRules: {
+        attr_name: [{ required: true, message: '请输入名称', trigger: 'blur' }]
+      },
+      // 编辑参数对话框
+      editParamDialogVisible: false,
+      // 编辑参数表单对象
+      editParamForm: {
+        attr_id: '',
+        attr_name: '',
+        attr_sel: ''
+      }
     }
   },
   computed: {
@@ -83,6 +187,21 @@ export default {
         return this.selectedKeys[2]
       }
       return null
+    },
+    addParamDiagTitle() {
+      if (this.activeName === 'many') {
+        return '动态参数'
+      } else {
+        return '静态属性'
+      }
+    },
+    // 编辑参数对话框标题
+    editParamDiagTitle() {
+      if (this.activeName === 'many') {
+        return '动态属性'
+      } else {
+        return '静态属性'
+      }
     }
   },
   created() {
@@ -121,18 +240,94 @@ export default {
     getCateAttrs() {
       // 只有选中了商品后才能发起请求
       if (this.selectedKeyId) {
-        this.$http.get(`categories/${this.selectedKeyId}/attributes`, {
-          params: { sel: this.activeName }
-        }).then(
-          res => {
+        this.$http
+          .get(`categories/${this.selectedKeyId}/attributes`, {
+            params: { sel: this.activeName }
+          })
+          .then(res => {
             if (res.data.meta.status === 200) {
               console.log(res.data.data)
+              if (this.activeName === 'many') {
+                this.manyTableData = res.data.data
+              } else {
+                this.onlyTableData = res.data.data
+              }
             } else {
               this.$message.error(res.data.meta.msg)
             }
-          }
-        )
+          })
       }
+    },
+    // 编辑参数
+    editParam(paramObj) {
+      console.log(paramObj)
+      this.editParamForm.attr_name = paramObj.attr_name
+      this.editParamForm.attr_id = paramObj.attr_id
+      this.editParamDialogVisible = true
+    },
+    // 添加参数
+    addParam() {
+      this.$refs.paramForm.validate((valid, err) => {
+        if (valid) {
+          this.$http
+            .post(`categories/${this.selectedKeyId}/attributes`, {
+              attr_name: this.paramForm.attr_name,
+              attr_sel: this.activeName
+            })
+            .then(res => {
+              if (res.data.meta.status === 201) {
+                this.$message.success('添加成功')
+                this.getCateAttrs()
+              } else {
+                this.$message.error(res.data.meta.msg)
+              }
+            })
+            .catch(err => {
+              this.message.error(err)
+            })
+        }
+        this.addParamDialogVisible = false
+      })
+    },
+    // 监听对话框关闭事件
+    handleCloseAddParamDiag() {
+      this.addParamDialogVisible = false
+      this.paramForm = ''
+      this.$refs.paramForm.restFileds()
+    },
+    // 监听关闭编辑参数对话框
+    handleCloseEditParamDiag() {
+      this.editParamForm = ''
+      this.$refs.editParamForm.resetFields()
+      this.editParamDialogVisible = false
+    },
+    // 提交修改请求
+    changeParam() {
+      this.$refs.editParamForm.validate(valid => {
+        if (valid) {
+          this.$http
+            .put(
+              `categories/${this.selectedKeyId}/attributes/${this.editParamForm.attr_id}`,
+              {
+                attr_name: this.editParamForm.attr_name,
+                attr_sel: this.activeName
+              }
+            )
+            .then(res => {
+              if (res.data.meta.status === 200) {
+                this.editParamDialogVisible = false
+                this.$message.success('修改成功')
+              } else {
+                this.$message.error(res.data.meta.msg)
+              }
+            })
+            .catch(err => {
+              console.log(err)
+              this.$message.error('网络异常')
+            })
+        } else {
+        }
+      })
     }
   }
 }
@@ -141,5 +336,10 @@ export default {
 <style lang="scss" scoped>
 .cat_opt {
   margin: 20px 0;
+}
+.el-tab-pane {
+  .el-button {
+    margin-bottom: 14px;
+  }
 }
 </style>
--- src/request/index.js
+++ src/request/index.js
@@ -1,11 +1,12 @@
 /*
  * @Author: robert zhang
  * @Date: 2020-04-22 07:16:08
- * @LastEditTime: 2020-05-03 01:51:22
+ * @LastEditTime: 2020-05-31 18:11:01
  * @LastEditors: robert zhang
  * @Description: 路由模块主文件
  */
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 const host = window.location.host
 const baseURL = `http://${host}/api/private/v1`
@@ -33,8 +34,38 @@ request.interceptors.request.use(
   },
   err => {
     console.log(err)
-    return Promise.reject(err)
+    Message.error({
+      message: '请求超时！'
+    })
+    return Promise.resolve(err)
   }
+)
+
+// 响应异常拦截
+axios.interceptors.response.use(res => {
+}, err => {
+  if (err && err.response) {
+    switch (err.response.status) {
+      case 400: err.message = '请求错误(400)'; break
+      case 401: err.message = '未授权，请重新登录(401)'; break
+      case 403: err.message = '拒绝访问(403)'; break
+      case 404: err.message = '请求出错(404)'; break
+      case 408: err.message = '请求超时(408)'; break
+      case 500: err.message = '服务器错误(500)'; break
+      case 501: err.message = '服务未实现(501)'; break
+      case 502: err.message = '网络错误(502)'; break
+      case 503: err.message = '服务不可用(503)'; break
+      case 504: err.message = '网络超时(504)'; break
+      case 505: err.message = 'HTTP版本不受支持(505)'; break
+      default: err.message = `连接出错(${err.response.status})!`
+    }
+  } else {
+    err.message = '连接服务器失败!'
+  }
+  Message.err({ message: err.message })
+  // 请求出错的时候执行Promise.resolve(err) 保证所有的请求无论成功失败都可以在回调中收到通知
+  return Promise.resolve(err)
+}
 )
 
 export default request
