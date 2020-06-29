f41aa4d9f3d7ff2a437a4076f34bba59c495dea1
robertzwj <robertzhangwenjie@gmail.com>
2020-06-29 16:34:46 +0800

    change environment to built-in variables

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: robert zhang
  # @Date: 2020-06-28 20:15:02
- # @LastEditTime: 2020-06-28 20:37:04
+ # @LastEditTime: 2020-06-29 14:39:59
  # @LastEditors: robert zhang
  # @Description: 部署脚本
  # @
@@ -9,16 +9,19 @@
 
 #!/bin/bash
 
+set -x
+
 echo "Starting deploy."
 
 # 如果服务正在运行先停止
-docker ps -a | grep ${projectName} > /dev/null &&  \
+sudo docker ps -a | grep ${projectName} > /dev/null &&  \
 echo "Stopping service" && \
 sudo docker stop ${projectName} && \
 echo "Removing service" && \
 sudo docker rm ${projectName}
 
 # 拉取新镜像并运行
+sudo docker image ls | grep 
 sudo docker rmi ${imageName}:${version}
 sudo docker run -itd --name=${projectName} ${imageName}:${version}
 echo "部署成功"
